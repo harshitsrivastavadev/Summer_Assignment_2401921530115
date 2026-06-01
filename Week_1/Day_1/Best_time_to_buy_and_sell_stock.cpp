a7f8d77ea3b540b3f8d4bf4106001f132afbff22
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-01 23:46:25 +0530

    Update Best_time_to_buy_and_sell_stock.cpp

--- Week_1/Day_1/Best_time_to_buy_and_sell_stock.cpp
+++ Week_1/Day_1/Best_time_to_buy_and_sell_stock.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = INT_MAX;
+        int profit = 0;
+        int maxProfit = 0;
+        for(int sell : prices){
+            if(sell < buy){
+                buy = sell;
+            } 
+            else {
+                profit = sell - buy;
+                maxProfit = max(profit, maxProfit);
+            }
+        }
+        return maxProfit;
+    }
+};
