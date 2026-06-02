b8cf3ac1081e532886380ba6ed03997fbd14e7de
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-02 23:27:08 +0530

    Update Maximum_average_subarray_I.cpp

--- Week_1/Day_2/Maximum_average_subarray_I.cpp
+++ Week_1/Day_2/Maximum_average_subarray_I.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        long sum=0;
+        for(int i=0;i<k;i++){
+            sum += nums[i];
+            }
+        long MAX=sum;
+        for(int i=k;i<nums.size();i++){
+            sum +=nums[i];
+            sum -=nums[i-k];
+            MAX=max(MAX,sum);
+        }
+        return double(MAX)/k;
+    }
+};
