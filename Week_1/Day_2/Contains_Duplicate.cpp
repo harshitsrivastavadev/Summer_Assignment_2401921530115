3d5d055fe1dcfb19f6de7db0d86423fa90880a7d
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-02 23:18:30 +0530

    Update Contains_Duplicate.cpp

--- Week_1/Day_2/Contains_Duplicate.cpp
+++ Week_1/Day_2/Contains_Duplicate.cpp
@@ -1,1 +1,30 @@
+// 2 methods through which we can solve it:-
 
+//use of sorting
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       for(int i=1;i<nums.size();i++){
+        if(nums[i]==nums[i-1]){
+            return true;
+        }
+       }
+       return false;
+    }
+};
+
+//use of hashmap
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int x :nums){
+            if(mp[x]>=1){
+                return true;
+            }
+            mp[x]++;
+        }
+        return false;
+    }
+};
