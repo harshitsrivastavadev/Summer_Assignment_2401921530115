a2a1ea9883f770d4c4d96db5821a034af007e5b5
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-03 22:29:50 +0530

    Update Squares_of_sorted_array.cpp

--- Week_1/Day_3/Squares_of_sorted_array.cpp
+++ Week_1/Day_3/Squares_of_sorted_array.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int i=0;
+        int j=nums.size()-1;
+        vector<int>temp;
+        while(i<=j){
+            temp.push_back(nums[i]*nums[i]);
+            i++;
+        }
+        sort(temp.begin(),temp.end());
+        return temp;
+    }
+};
