c63a8cb809002f2ea360ad69de59162ad73c280d
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-04 21:11:49 +0530

    Update Spiral_Matrix.cpp

--- Week_1/Day_4/Spiral_Matrix.cpp
+++ Week_1/Day_4/Spiral_Matrix.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int rs=0;
+        int re=matrix.size()-1;
+        int cs=0;
+        int ce=matrix[0].size()-1;
+        vector<int>v;
+        while(rs<=re && cs<=ce){
+            for(int i=cs;i<=ce;i++){
+                v.push_back(matrix[rs][i]);
+            }
+            rs++;
+            if(rs>re || cs>ce) break;
+            for(int i=rs;i<=re;i++){
+                v.push_back(matrix[i][ce]);
+            }
+            ce--;
+             if(rs>re || cs>ce) break;
+            for(int i=ce;i>=cs;i--){
+                v.push_back(matrix[re][i]);
+            }
+            re--;
+             if(rs>re || cs>ce) break;
+            for(int i=re;i>=rs;i--){
+                v.push_back(matrix[i][cs]);
+            }
+            cs++;
+        }
+        return v;
+    }
+};
