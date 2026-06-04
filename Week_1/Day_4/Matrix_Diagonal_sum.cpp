6f904305ebe12f30cb6a451d423a79f690dcbb75
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-04 21:07:21 +0530

    Update Matrix_Diagonal_sum.cpp

--- Week_1/Day_4/Matrix_Diagonal_sum.cpp
+++ Week_1/Day_4/Matrix_Diagonal_sum.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int length = mat.size();
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += mat[i][i];          
+            sum += mat[length-i-1][i]; 
+            if (length%2== 1 && i== length/2) 
+                sum -= mat[i][i];
+        }
+        return sum;
+    }
+};
