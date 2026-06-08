e54ebc70a36ab7d7cbf111fa1eeb279c041e0c35
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-08 23:25:56 +0530

    Update Valid_Anagram.cpp

--- Week_2/Day_1/Valid_Anagram.cpp
+++ Week_2/Day_1/Valid_Anagram.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto a : mp){
+            if(a.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
