8ffea33ec7e04434b04f6759d1b47f80f1f4fe6f
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-08 23:42:52 +0530

    Update Ransom_note.cpp

--- Week_2/Day_1/Ransom_note.cpp
+++ Week_2/Day_1/Ransom_note.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp;
+        for(char c: magazine){
+            mp[c]++;
+        }
+        for(char s:ransomNote){
+            if(mp[s]==0){
+                return false;
+            }
+            mp[s]--;
+        }
+        return true;
+    }
+};
