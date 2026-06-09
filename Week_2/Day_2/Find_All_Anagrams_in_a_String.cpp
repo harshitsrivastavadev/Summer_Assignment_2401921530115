e2a56a8137d3c6a55145b0712fe8ea8540e967be
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-10 00:03:26 +0530

    Update Find_All_Anagrams_in_a_String.cpp

--- Week_2/Day_2/Find_All_Anagrams_in_a_String.cpp
+++ Week_2/Day_2/Find_All_Anagrams_in_a_String.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        unordered_map<char,int> p2,s2;
+        vector<int> ans;
+        for(int i=0;i<p.size();i++){
+            p2[p[i]]++;  
+            s2[s[i]]++;
+        }
+        if(p2 == s2){
+            ans.push_back(0); 
+        }
+        int j=0;  
+        for(int i=p.size();i<s.size();i++){
+            s2[s[i]]++; 
+            s2[s[j]]--;
+            if(s2[s[j]] == 0) s2.erase(s[j]);
+            j++;
+            if(p2 == s2) ans.push_back(j);
+        }
+        return ans;
+    }
+};
