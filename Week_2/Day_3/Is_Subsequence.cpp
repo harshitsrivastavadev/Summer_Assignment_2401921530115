0e95ce27420a1a20416cf0d1580c8ac5fe245026
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-10 23:40:01 +0530

    Update Is_Subsequence.cpp

--- Week_2/Day_3/Is_Subsequence.cpp
+++ Week_2/Day_3/Is_Subsequence.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int a=0;
+        int b=0;
+        while(a<s.size() && b<t.size()){
+            if(s[a]==t[b]){
+                a++;
+            }
+            b++;
+        }
+        return a==s.size();
+    }
+};
