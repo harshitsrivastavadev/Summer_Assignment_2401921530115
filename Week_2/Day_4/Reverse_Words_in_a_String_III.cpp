1d58093c7022441a36436f75889f3550ab9b91c3
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-12 00:01:00 +0530

    Update Reverse_Words_in_a_String_III.cpp

--- Week_2/Day_4/Reverse_Words_in_a_String_III.cpp
+++ Week_2/Day_4/Reverse_Words_in_a_String_III.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    void reverseWord(string &s,int start,int end) {
+        while(start<end){
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+    }
+    string reverseWords(string s) {
+        int start = 0;
+        for (int i=0;i<= s.length();i++){
+            if (i == s.length() || s[i]==' ') {
+                reverseWord(s,start,i-1);
+                start=i+1;
+            }
+        }
 
+        return s;
+    }
+};
