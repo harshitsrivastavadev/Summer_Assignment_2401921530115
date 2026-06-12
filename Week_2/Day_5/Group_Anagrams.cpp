96e0d7f4c638e58b3cc9b74beeed7de34d68fd9c
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-12 23:14:40 +0530

    Update Group_Anagrams.cpp

--- Week_2/Day_5/Group_Anagrams.cpp
+++ Week_2/Day_5/Group_Anagrams.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+        vector<string>a = strs;
+        for(int i=0;i<strs.size();i++){
+            sort(a[i].begin(),a[i].end());
+        }
+        for(int i=0;i<strs.size();i++){
+            mp[a[i]].push_back(strs[i]);
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
