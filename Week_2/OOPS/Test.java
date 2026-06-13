84f51dda5f161733687cce488071574207dbfc77
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-13 23:32:38 +0530

    Update Test.java

--- Week_2/OOPS/Test.java
+++ Week_2/OOPS/Test.java
@@ -4,20 +4,16 @@ import music.string.Veena;
 import music.wind.Saxophone;
 public class Test {
     public static void main(String[] args) {
-      
         // a. Veena object
         Veena v = new Veena();
         v.play();
-      
         // b. Saxophone object
         Saxophone s = new Saxophone();
         s.play();
-      
         // c. Using interface reference
         Playable p;
         p = new Veena();
-        p.play();
-      
+        p.play();      
         p = new Saxophone();
         p.play();
     }
