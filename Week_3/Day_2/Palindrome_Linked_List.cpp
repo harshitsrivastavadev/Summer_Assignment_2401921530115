7ce36146461b21d209559147a280389c23de5f34
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-16 23:58:06 +0530

    Update Palindrome_Linked_List.cpp

--- Week_3/Day_2/Palindrome_Linked_List.cpp
+++ Week_3/Day_2/Palindrome_Linked_List.cpp
@@ -1,1 +1,52 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if( head==NULL || head->next==NULL){
+            return true;
+        }
+        int count=0;
+        ListNode*temp=head;
+        while(temp){
+            count++;
+            temp=temp->next;
+        }
+        count/=2;
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        while(count--){
+            prev=curr;
+            curr=curr->next;
+        }
+        prev->next=NULL;
+        ListNode*front;
+        prev=NULL;
+        while(curr){
+            front=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=front;
+        }
+        ListNode*H1=head;
+        ListNode*H2=prev;
+        while(H1 && H2){
+            if(H1->val!=H2->val){
+                return false;
+            }
+            else{
+                H1=H1->next;
+                H2=H2->next;
+            }
+        }
+        return true;
+    }
+};
