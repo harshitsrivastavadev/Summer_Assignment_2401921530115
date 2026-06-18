1bf10875501eaf55da92656cc0ca47c248f37918
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-18 22:28:52 +0530

    Update Evaluate_Reverse_Polish_Notation.cpp

--- Week_3/Day_4/Evaluate_Reverse_Polish_Notation.cpp
+++ Week_3/Day_4/Evaluate_Reverse_Polish_Notation.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+         stack<int> st;
+         for(auto x: tokens){
+            if (x == "+"|| x == "-"|| x== "*"||x== "/"){
+                int s = st.top();
+                st.pop();
+                int f = st.top();
+                st.pop();
+                if (x=="+") st.push(f+s);
+                else if(x=="-") st.push(f-s);
+                else if(x=="*") st.push(f*s);
+                else if(x=="/") st.push(f/s);
+            }
+            else{
+                st.push(stoi(x));
+            }
+        }
+        return st.top();
+    }
+};
