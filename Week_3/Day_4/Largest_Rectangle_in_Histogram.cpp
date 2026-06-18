c50bfcf98e7f3d1b3077a1c9e160a280ca8a296c
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-18 22:36:08 +0530

    Update Largest_Rectangle_in_Histogram.cpp

--- Week_3/Day_4/Largest_Rectangle_in_Histogram.cpp
+++ Week_3/Day_4/Largest_Rectangle_in_Histogram.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        vector<int>PSE(n);  
+        vector<int>NSE(n); 
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+            PSE[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+        st=stack<int>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+            NSE[i]= st.empty()?n:st.top();
+            st.push(i);
+        }
+        int MAX= 0;
+        for(int i=0;i<n;i++){
+            int area=heights[i]*(NSE[i]-PSE[i]-1);
+            MAX= max(MAX, area);
+        }
+        return MAX;
+    }
+};
