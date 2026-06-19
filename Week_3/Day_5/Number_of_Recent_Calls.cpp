3b312e56b2fa77bd63d9bbb1d84fa06ade64db93
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-19 23:51:40 +0530

    Update Number_of_Recent_Calls.cpp

--- Week_3/Day_5/Number_of_Recent_Calls.cpp
+++ Week_3/Day_5/Number_of_Recent_Calls.cpp
@@ -1,1 +1,15 @@
+class RecentCounter {
+public:
+    RecentCounter() {
+        
+    }
+    queue<int> q;
+    int ping(int t) {
+        q.push(t);
+        while(q.front() < t-3000){
+            q.pop();
+        }
+        return q.size();
+    }
+};
 
