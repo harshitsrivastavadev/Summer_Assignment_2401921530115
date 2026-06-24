6076184ad3a02b5cc6dcf99816affa8d23c3292a
Harshit Srivastava <harshit02012007@gmail.com>
2026-06-24 23:59:39 +0530

    Update Validate_Binary_Search_Tree.cpp

--- Week_4/Day_3/Validate_Binary_Search_Tree.cpp
+++ Week_4/Day_3/Validate_Binary_Search_Tree.cpp
@@ -1,1 +1,28 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    bool result = true;
+    void helper(TreeNode* root){
+        if(root == NULL)return;
+        helper(root->left);
+        if(prev == NULL)prev = root;
+        else if(root->val <= prev->val) result=false;
+        else prev = root;
+        helper(root->right);
+    }
+    bool isValidBST(TreeNode* root) {
+        helper(root);
+        return result;
+    }
+};
